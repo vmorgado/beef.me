43a1bf4c602f741dfd13bbf6f905ab59a14f5a86
Vitor Morgado <vitor.morgado@tourstream.eu>
2018-02-22 17:57:44 +0100

    added html part of navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,31 @@
 <template>
   <div id="app">
+    <nav class="navbar navbar-light bg-light justify-content-between">
+      <a class="navbar-brand">Beef.me</a>
+
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item active">
+          <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Pricing</a>
+        </li>
+      </ul>
+
+      <form class="form-inline">
+        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+      </form>
+    </nav>
     <router-view/>
   </div>
 </template>
 
 <script>
+import 'bootstrap'
 export default {
   name: 'App'
 }
@@ -12,7 +33,7 @@ export default {
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
