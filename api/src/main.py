95d8470abe1bea6af4f509b1792407d54f338337
Vitor Morgado <vitorz2k7@gmail.com>
2018-03-02 21:37:54 +0100

    falcon up and running

--- api/src/main.py
+++ api/src/main.py
@@ -2,12 +2,17 @@ import falcon
 
 
 class HelloWorldResource:
-
     def on_get(self, request, response):
+        response.media = ('Hello World from Falcon API')
 
-        response.media = ('Hello World from Falcon Python 3.6 app with' +
-                          ' Gunicorn running in a container.')
-
+class BeefResource:
+    def on_get(self, request, response):
+        response.media = ('beef')
+class UserResource:
+    def on_get(self, request, response):
+        response.media = ('user')
 
 app = falcon.API()  
 app.add_route('/', HelloWorldResource())
+app.add_route('/beef', BeefResource())
+app.add_route('/user', UserResource())
