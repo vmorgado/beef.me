12aa34621158f7e826a72f1633e51f09640dea7b
Vitor Morgado <vitor.morgado@inpera.net>
2022-11-28 14:03:21 +0100

    Adds svelte-navigator + adds base routing

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,36 +1,27 @@
 <script>
-    import Button from "@smui/button";
-    import Card, { Content, Actions } from "@smui/card";
-    import { Label, Icon } from "@smui/common";
-
-    let name = "";
-    let showCard = false;
+   import { Router, Route, Link } from "svelte-navigator";
+   import Home from "./routes/Home.svelte";
+   import Register from "./routes/Register.svelte";
 </script>
 
-<main>
-    <Card style="width: 360px; margin: 2em auto;">
-        <Content />
-        <Actions>
-            <Button
-                href="https://github.com/hperrin/svelte-material-ui"
-                target="_blank"
-                style="text-decoration: none;"
-            >
-                <Label>GitHub Repo</Label>
-            </Button>
-            <Button
-                href="https://sveltematerialui.com/"
-                target="_blank"
-                style="text-decoration: none;"
-            >
-                <Label>Website</Label>
-            </Button>
-        </Actions>
-    </Card>
-</main>
+<Router>
+    <nav>
+        <Link to="/">Home</Link>
+        <Link to="/register">Register</Link>
+    </nav>
+    <div>
+        <Route path="/">
+            <Home />
+        </Route>
+
+        <Route path="/register">
+            <Register />
+        </Route>
+    </div>
+
+</Router>
 
 <svelte:head>
-    <!-- Fonts -->
     <link
         rel="stylesheet"
         href="https://fonts.googleapis.com/icon?family=Material+Icons"
@@ -39,14 +30,10 @@
         rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
     />
-
-    <!-- Material Typography -->
     <link
         rel="stylesheet"
         href="https://unpkg.com/@material/typography@14.0.0/dist/mdc.typography.css"
     />
-
-    <!-- SMUI -->
     <link
         rel="stylesheet"
         href="https://unpkg.com/svelte-material-ui/bare.css"
---
+++ frontend/src/routes/Home.svelte
@@ -0,0 +1,33 @@
+
+<main>
+</main>
+<script>
+
+    import Button from "@smui/button";
+    import Card, { Content, Actions } from "@smui/card";
+    import { Label, Icon } from "@smui/common";
+
+    let name = "";
+    let showCard = false;
+</script>
+<main>
+    <Card style="width: 360px; margin: 2em auto;">
+        <Content />
+        <Actions>
+            <Button
+                href="https://github.com/hperrin/svelte-material-ui"
+                target="_blank"
+                style="text-decoration: none;"
+            >
+                <Label>GitHub Repo</Label>
+            </Button>
+            <Button
+                href="https://sveltematerialui.com/"
+                target="_blank"
+                style="text-decoration: none;"
+            >
+                <Label>Website</Label>
+            </Button>
+        </Actions>
+    </Card>
+</main>
