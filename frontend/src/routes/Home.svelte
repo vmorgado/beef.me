54d4e7cfc0efc8c7cd70d6a584e9c77e5c2935e6
Vitor Morgado <vitor.morgado@inpera.net>
2022-11-28 16:39:01 +0100

    starts setting up base pages

--- frontend/src/routes/Home.svelte
+++ frontend/src/routes/Home.svelte
@@ -1,33 +1,9 @@
-
-<main>
-</main>
 <script>
+    import LayoutGrid from "@smui/layout-grid";
 
-    import Button from "@smui/button";
-    import Card, { Content, Actions } from "@smui/card";
-    import { Label, Icon } from "@smui/common";
-
-    let name = "";
-    let showCard = false;
+    import Beef from "../lib/Beef.svelte";
 </script>
-<main>
-    <Card style="width: 360px; margin: 2em auto;">
-        <Content />
-        <Actions>
-            <Button
-                href="https://github.com/hperrin/svelte-material-ui"
-                target="_blank"
-                style="text-decoration: none;"
-            >
-                <Label>GitHub Repo</Label>
-            </Button>
-            <Button
-                href="https://sveltematerialui.com/"
-                target="_blank"
-                style="text-decoration: none;"
-            >
-                <Label>Website</Label>
-            </Button>
-        </Actions>
-    </Card>
-</main>
+
+<LayoutGrid>
+    <Beef />
+</LayoutGrid>
