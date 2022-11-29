75432bca9093d6757202af8b31543c8f62f4ef9f
Vitor Morgado <vitor.morgado@inpera.net>
2022-11-30 00:10:55 +0100

    Sets up first frontend mockup with material

--- frontend/src/lib/Beef.svelte
+++ frontend/src/lib/Beef.svelte
@@ -1,84 +1,51 @@
-<script lang="ts">
-    import Card, {
-        Content,
-        PrimaryAction,
-        Media,
-        MediaContent,
-        Actions,
-        ActionButtons,
-        ActionIcons,
-    } from "@smui/card";
-    import Button, { Label } from "@smui/button";
-    import IconButton, { Icon } from "@smui/icon-button";
-    import Cell from "@smui/layout-grid";
+<script>
+	import {
+		Button,
+		Container,
+		Row,
+		Col,
+		Card,
+		CardTitle,
+		CardActions,
+		Icon,
+	} from "svelte-materialify";
+	import { onMount } from "svelte";
+	import {
+		mdiShareVariant,
+		mdiThumbDownOutline,
+		mdiFire,
+		mdiDisc,
+	} from "@mdi/js";
 
-    let clicked = 0;
+	let player;
+
+	onMount(() => {
+		console.log(player);
+	});
 </script>
 
-<Cell span={12}>
-    <Card>
-        <PrimaryAction on:click={() => clicked++}>
-            <Media class="card-media-16x9" aspectRatio="16x9">
-                <MediaContent>
-                    <div
-                        style="color: #fff; position: absolute; bottom: 16px; left: 16px;"
-                    >
-                        <h2
-                            class="mdc-typography--headline6"
-                            style="margin: 0;"
-                        >
-                            A card with media.
-                        </h2>
-                        <h3
-                            class="mdc-typography--subtitle2"
-                            style="margin: 0;"
-                        >
-                            And a subtitle.
-                        </h3>
-                    </div>
-                </MediaContent>
-            </Media>
-            <Content class="mdc-typography--body2">
-                It's all in this card. It's a veritable smorgasbord of card
-                features.
-            </Content>
-        </PrimaryAction>
-        <Actions>
-            <ActionButtons>
-                <Button on:click={() => clicked++}>
-                    <Label>Action</Label>
-                </Button>
-                <Button on:click={() => clicked++}>
-                    <Label>Another</Label>
-                </Button>
-            </ActionButtons>
-            <ActionIcons>
-                <IconButton
-                    on:click={() => clicked++}
-                    toggle
-                    aria-label="Add to favorites"
-                    title="Add to favorites"
-                >
-                    <Icon class="material-icons" on>favorite</Icon>
-                    <Icon class="material-icons">favorite_border</Icon>
-                </IconButton>
-                <IconButton
-                    class="material-icons"
-                    on:click={() => clicked++}
-                    title="Share">share</IconButton
-                >
-                <IconButton
-                    class="material-icons"
-                    on:click={() => clicked++}
-                    title="More options">more_vert</IconButton
-                >
-            </ActionIcons>
-        </Actions>
-    </Card>
-</Cell>
-
-<style>
-    * :global(.card-media-16x9) {
-        background-image: url(https://place-hold.it/320x180?text=16x9&fontsize=23);
-    }
-</style>
+<Card>
+	<CardTitle>Vitor Morgado</CardTitle>
+	<Container>
+		<Row>
+			<Col colls={12}>
+				<audio bind:this={player} id="player" controls />
+			</Col>
+		</Row>
+	</Container>
+	<CardActions>
+		<Button class="pink-text mr-2">
+			<Icon class="mr-2" path={mdiFire} /> Lit
+		</Button>
+		<Button class="indigo-text mr-2">
+			<Icon class="mr-2" path={mdiThumbDownOutline} /> Whack
+		</Button>
+		<Button class="black-text">
+			<Icon class="mr-2" path={mdiDisc} /> Remix
+		</Button>
+		<div style="flex-grow:1" />
+		<Button right icon class="blue-text">
+			<Icon path={mdiShareVariant} />
+		</Button>
+	</CardActions>
+</Card>
--- frontend/src/routes/Home.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-    import LayoutGrid from "@smui/layout-grid";
-
-    import Beef from "../lib/Beef.svelte";
-</script>
-
-<LayoutGrid>
-    <Beef />
-</LayoutGrid>
